5ecb43be2359aecfd2041fde6d688da633228fa1
Ashwini Purohit <ashwinipurohit@yahoo.in>
2016-01-12 23:49:28 +0530

    User Management Setup with Admin User System

---
+++ profile.php
@@ -0,0 +1,10 @@
+<?php
+require_once("models/config.php");
+if (!securePage($_SERVER['PHP_SELF'])){die();}
+
+//Prevent the user visiting the logged in page if he is not logged in
+if(!isUserLoggedIn()) { header("Location: login.php"); die(); }
+
+require_once("models/header.php");
+
+echo $loggedInUser->email;
