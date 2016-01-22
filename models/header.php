685123d9c740856059521400ffa0db059f1e0e25
Rishi Raj <rishiraj824@gmail.com>
2016-01-22 09:19:36 +0530

    new changes

--- models/header.php
+++ models/header.php
@@ -15,117 +15,19 @@ echo "
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800,300%7C' rel='stylesheet' type='text/css'>
 
     <title>ACM Coding Event | Just another WordPress site</title><!-- 
-    <link rel='alternate' type='application/rss+xml' title='Signature Responsive Wordpress Theme &raquo; Feed' href='http://ninetheme.com/themes/signature/index.php/feed/' />
-    <link rel='alternate' type='application/rss+xml' title='Signature Responsive Wordpress Theme &raquo; Comments Feed' href='http://ninetheme.com/themes/signature/index.php/comments/feed/' />
-    <link rel='alternate' type='application/rss+xml' title='Signature Responsive Wordpress Theme &raquo; frontpage Comments Feed' href='http://ninetheme.com/themes/signature/index.php/sample-page-2-2/feed/' /> -->
-    <script type='text/javascript'>
-        window._wpemojiSettings = {
-            'baseUrl': 'http:\/\/s.w.org\/images\/core\/emoji\/72x72\/',
-            'ext': '.png',
-            'source': {
-                'concatemoji': 'http:\/\/ninetheme.com\/themes\/signature\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.3.2'
-            }
-        };
-        ! function(a, b, c) {
-            function d(a) {
-                var c = b.createElement('canvas'),
-                    d = c.getContext && c.getContext('2d');
-                return d && d.fillText ? (d.textBaseline = 'top', d.font = '600 32px Arial', 'flag' === a ? (d.fillText(String.fromCharCode(55356, 56812, 55356, 56807), 0, 0), c.toDataURL().length > 3e3) : (d.fillText(String.fromCharCode(55357, 56835), 0, 0), 0 !== d.getImageData(16, 16, 1, 1).data[0])) : !1
-            }
+   <!-- Latest compiled and minified CSS -->
+   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' crossorigin='anonymous'>
 
-            function e(a) {
-                var c = b.createElement('script');
-                c.src = a, c.type = 'text/javascript', b.getElementsByTagName('head')[0].appendChild(c)
-            }
-            var f, g;
-            c.supports = {
-                simple: d('simple'),
-                flag: d('flag')
-            }, c.DOMReady = !1, c.readyCallback = function() {
-                c.DOMReady = !0
-            }, c.supports.simple && c.supports.flag || (g = function() {
-                c.readyCallback()
-            }, b.addEventListener ? (b.addEventListener('DOMContentLoaded', g, !1), a.addEventListener('load', g, !1)) : (a.attachEvent('onload', g), b.attachEvent('onreadystatechange', function() {
-                'complete' === b.readyState && c.readyCallback()
-            })), f = c.source || {}, f.concatemoji ? e(f.concatemoji) : f.wpemoji && f.twemoji && (e(f.twemoji), e(f.wpemoji)))
-        }(window, document, window._wpemojiSettings);
-    </script>
-    <style type='text/css'>
-        img.wp-smiley,
-        img.emoji {
-            display: inline !important;
-            border: none !important;
-            box-shadow: none !important;
-            height: 1em !important;
-            width: 1em !important;
-            margin: 0 .07em !important;
-            vertical-align: -0.1em !important;
-            background: none !important;
-            padding: 0 !important;
-        }
-    </style>
-    <link rel='stylesheet' id='contact-form-7-css' href='http://ninetheme.com/themes/signature/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=4.3.1' type='text/css' media='all' />
-    <link rel='stylesheet' id='ninetheme_signature_cssbootstrap-css' href='http://ninetheme.com/themes/signature/wp-content/themes/signature/css/main.min.css?ver=1.0' type='text/css' media='all' />
-    <link rel='stylesheet' id='ninetheme_signature_awesome-css' href='http://ninetheme.com/themes/signature/wp-content/themes/signature/css/custom.css?ver=1.0' type='text/css' media='all' />
-    <link rel='stylesheet' id='ninetheme_signature_wordpress-style-css' href='http://ninetheme.com/themes/signature/wp-content/themes/signature/css/wordpress.css?ver=1.0' type='text/css' media='all' />
-    <link rel='stylesheet' id='ninetheme_signature_flexslidercss-css' href='http://ninetheme.com/themes/signature/wp-content/themes/signature/css/flexslider.css?ver=1.0' type='text/css' media='all' />
-    <link rel='stylesheet' id='ninetheme_signature_fonts-load-css' href='//fonts.googleapis.com/css?family=Open%2BSans%7CLato%7CRoboto%3A700%2C300%2C600%2C400%2C500&#038;ver=1.0.0' type='text/css' media='all' />
-    <link rel='stylesheet' id='style-css' href='http://ninetheme.com/themes/signature/wp-content/themes/signature/style.css?ver=4.3.2' type='text/css' media='all' />
-    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
-    <link rel='stylesheet' id='js_composer_front-css' href='//ninetheme.com/themes/signature/wp-content/uploads/js_composer/js_composer_front_custom.css?ver=4.8.1' type='text/css' media='all' />
-    <script type='text/javascript' src='http://ninetheme.com/themes/signature/wp-includes/js/jquery/jquery.js?ver=1.11.3'></script>
-    <script type='text/javascript' src='http://ninetheme.com/themes/signature/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
-    <link rel='EditURI' type='application/rsd+xml' title='RSD' href='http://ninetheme.com/themes/signature/xmlrpc.php?rsd' />
-    <link rel='wlwmanifest' type='application/wlwmanifest+xml' href='http://ninetheme.com/themes/signature/wp-includes/wlwmanifest.xml' />
-    <meta name='generator' content='WordPress 4.3.2' />
-    <link rel='canonical' href='http://ninetheme.com/themes/signature/' />
-    <link rel='shortlink' href='http://ninetheme.com/themes/signature/' />
+   <!-- Optional theme -->
+   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css' integrity='sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r' crossorigin='anonymous'>
 
-    <!-- Font -->
-
-    <style>
-        .navigation-container {
-            background-color: #171717;
-        }
-        
-        .navigation-container nav .nav-inner li a {
-            color: #fff;
-        }
-        
-        .navigation-container nav .nav-inner li a:hover {
-            color: #fff200;
-        }
-        
-        .fullscreen {
-            background-color: #171717;
-        }
-    </style>
-
-    <style type='text/css'>
-        .recentcomments a {
-            display: inline !important;
-            padding: 0 !important;
-            margin: 0 !important;
-        }
-    </style>
-    <meta name='generator' content='Powered by Visual Composer - drag and drop page builder for WordPress.' />
-    <!--[if lte IE 9]><link rel='stylesheet' type='text/css' href='http://ninetheme.com/themes/signature/wp-content/plugins/vc_composer_4_8_1/assets/css/vc_lte_ie9.min.css' media='screen'><![endif]-->
-    <!--[if IE  8]><link rel='stylesheet' type='text/css' href='http://ninetheme.com/themes/signature/wp-content/plugins/vc_composer_4_8_1/assets/css/vc-ie8.min.css' media='screen'><![endif]-->
-    <style type='text/css' data-type='vc_shortcodes-custom-css'>
-        .vc_custom_1451667456399 {
-            background-image: url(background.jpeg) !important;
-        }
-    </style>
-    <noscript>
-        <style type='text/css'>
-            .wpb_animate_when_almost_visible {
-                opacity: 1;
-            }
-        </style>
-    </noscript>
+   <!-- Latest compiled and minified JavaScript -->
+   <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' integrity='sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS' crossorigin='anonymous'></script>
 
 <link href=''.$template.'' rel='stylesheet' type='text/css' />
 <link href='models/site-templates/default.css' rel='stylesheet' type='text/css' />
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
 <script src='models/funcs.js' type='text/javascript'>
 </script>
 </head>
