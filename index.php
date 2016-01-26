63099ed65ea779080bf56d7934e53c92808dc88c
Rishi Raj <rishiraj824@gmail.com>
2016-01-26 08:46:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,5 @@
 http:// .com
 */
 
-require_once("models/config.php");
-if (!securePage($_SERVER['PHP_SELF'])){die();}
-require_once("models/header.php");
-
-echo "
-
-<div id='left-nav'>";
-include("left-nav.php");
-
-echo "
-</div>
-<div id='main'>
-
-</div>
-<div id='bottom'></div>
-</div>
-</body>
-</html>";
-
+header ('Location:Website/ACM Site/codart');
 ?>
